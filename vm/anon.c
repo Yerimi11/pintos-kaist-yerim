5013e875702f6aaf3d93c5ed1e65f48f7330f97c
Ubuntu <ubuntu@ip-172-31-43-76.ap-northeast-2.compute.internal>
2022-01-23 16:05:40 +0000

    swap_make_succ_checkerror

--- vm/anon.c
+++ vm/anon.c
@@ -2,6 +2,7 @@
 
 #include "vm/vm.h"
 #include "devices/disk.h"
+#include "include/lib/kernel/bitmap.h"
 
 /* DO NOT MODIFY BELOW LINE */
 static struct disk *swap_disk;
@@ -26,10 +27,6 @@ vm_anon_init (void) {
 	/* TODO: Set up the swap_disk. */
 	swap_disk = disk_get(1, 1);
 
-#ifdef DBG
-	printf("disk size : %d\n", disk_size(swap_disk));
-#endif
-
 	bitcnt = disk_size(swap_disk)/SECTORS_IN_PAGE; // #ifdef Q. disk size decided by swap-size option? 
 	swap_table = bitmap_create(bitcnt); // each bit = swap slot for a frame
 }
@@ -52,12 +49,59 @@ anon_initializer (struct page *page, enum vm_type type, void *kva) {
 static bool
 anon_swap_in (struct page *page, void *kva) {
 	struct anon_page *anon_page = &page->anon;
+
+	int swap_sec = anon_page->swap_sec;
+	int swap_slot_idx = swap_sec / SECTORS_IN_PAGE;
+
+	if(bitmap_test(swap_table, swap_slot_idx) == 0)
+		//return false;
+		PANIC("(anon swap in) Frame not stored in the swap slot!\n");
+
+	page->frame->kva = kva;
+
+	bitmap_set(swap_table, swap_slot_idx, 0);
+
+	// ASSERT(is_writable(kva) != false);
+
+	// disk_read is done per sector; repeat until one page is read
+	for(int sec = 0; sec < SECTORS_IN_PAGE; sec++)
+		disk_read(swap_disk, swap_sec + sec, page->frame->kva + DISK_SECTOR_SIZE * sec);
+	
+	// restore vaddr connection
+	pml4_set_page(thread_current()->pml4, page->va, kva, true); // writable true, as we are writing into the frame
+
+	anon_page->swap_sec = -1;
+	return true;
 }
 
 /* Swap out the page by writing contents to the swap disk. */
 static bool
 anon_swap_out (struct page *page) {
 	struct anon_page *anon_page = &page->anon;
+
+	// Find free slot in swap disk
+	// Need at least PGSIZE to store frame into the slot 
+	// size_t free_idx = bitmap_scan(swap_table, 0, SECTORS_IN_PAGE, 0);
+	size_t free_idx = bitmap_scan_and_flip(swap_table, 0, 1, 0);
+
+	if(free_idx == BITMAP_ERROR)
+		PANIC("(anon swap-out) No more free swap slots!\n");
+
+	int swap_sec = free_idx * SECTORS_IN_PAGE;
+
+	// disk_write is done per sector; repeat until one page is written
+	for(int sec = 0; sec < SECTORS_IN_PAGE; sec++)
+		disk_write(swap_disk, swap_sec + sec, page->frame->kva + DISK_SECTOR_SIZE * sec);
+
+	// access to page now generates fault
+	pml4_clear_page(thread_current()->pml4, page->va);
+
+	anon_page->swap_sec = swap_sec;
+
+	page->frame->page = NULL;
+	page->frame = NULL;
+
+	return true;
 }
 
 /* Destroy the anonymous page. PAGE will be freed by the caller. */
